825e7ce8ec38e98828b9d03f924bcc002d269850
mostovyi <93921260+mostovyi@users.noreply.github.com>
2021-12-21 14:38:38 +0200

    Add files via upload
    Initial commit

---
+++ src/test/java/com/BotsCrewTestProject/BotsCrewTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BotsCrewTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsCrewTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
